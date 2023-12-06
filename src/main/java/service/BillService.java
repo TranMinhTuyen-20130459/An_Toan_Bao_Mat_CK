4d6ad1c962eac17baa312f6c9ae3d38b140a5fee
Trần Tuyên <20130459@st.hcmuaf.edu.vn>
2023-12-07 00:36:01 +0700

    tiếp tục chỉnh sửa hàm getAllInfoBill() trong class BillService

--- src/main/java/service/BillService.java
+++ src/main/java/service/BillService.java
@@ -1,8 +1,13 @@
 package service;
 
 import database.dao.BillDAO;
+import database.dao.PublicKeyDAO;
 import model.reponse.InfoBillResponse;
+import utils.AsymmetricEncrypt;
+import utils.HashUtil;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,37 +15,63 @@ import java.util.stream.Collectors;
 public class BillService {
 
     public static List<InfoBillResponse> getAllInfoBill() {
-        BillDAO dao = new BillDAO();
+
+        var billDAO = new BillDAO();
+        var publicKeyDAO = new PublicKeyDAO();
+        var rsa = new AsymmetricEncrypt(AsymmetricEncrypt.RSA);
+
         try {
-            var list_bill = dao.getAllBill();
+            var list_bill = billDAO.getAllBill();
 
             // Sử dụng stream API trong Java 8 để map các thuộc tính trong Bill sang InfoBillResponse
             return list_bill.stream()
-                    .map(item -> {
+                    .map(bill -> {
 
                         var security_status = "Hợp lệ";
+                        var hash_bill_encrypted = bill.getHash_bill_encrypted();
+
+                        try {
+
+                            // cần set về null bởi vì trước khi Đặt Hàng thì hash_bill_encrypted bằng null
+                            bill.setHash_bill_encrypted(null);
+                            var hash_bill = HashUtil.hashText(bill.toString(), HashUtil.SHA_1);
+
+                            // Lấy ra publicKey tương ứng để giải mã hash_bill_encrypted
+                            var pk = publicKeyDAO.getPublicKeyByInfoBill(bill);
+
+                            rsa.importPublicKey(pk.getPublic_key());
+
+                            var hash_bill_decrypted = rsa.decryptFromBase64(hash_bill_encrypted, AsymmetricEncrypt.transformation_RSA);
+
+                            if (!hash_bill.equals(hash_bill_decrypted)) security_status = "Đã bị chỉnh sửa";
+
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
 
                         return InfoBillResponse.builder()
-                                .id_bill(item.getId_bill())
-                                .id_user(item.getId_user())
-                                .id_status_bill(item.getId_status_bill())
-                                .id_city(item.getId_city())
-                                .name_customer(item.getName_customer())
-                                .phone_customer(item.getPhone_customer())
-                                .email_customer(item.getEmail_customer())
-                                .address_customer(item.getAddress_customer())
-                                .bill_price(item.getBill_price())
-                                .total_price(item.getTotal_price())
-                                .time_order(item.getTime_order())
+                                .id_bill(bill.getId_bill())
+                                .id_user(bill.getId_user())
+                                .id_status_bill(bill.getId_status_bill())
+                                .id_city(bill.getId_city())
+                                .name_customer(bill.getName_customer())
+                                .phone_customer(bill.getPhone_customer())
+                                .email_customer(bill.getEmail_customer())
+                                .address_customer(bill.getAddress_customer())
+                                .bill_price(bill.getBill_price())
+                                .total_price(bill.getTotal_price())
+                                .time_order(bill.getTime_order())
                                 .security_status(security_status)
                                 .build();
                     })
                     .collect(Collectors.toList());
 
         } catch (Exception e) {
+
+            e.printStackTrace();
             return new ArrayList<>();
         } finally {
-            if (dao.connectDB != null) dao.connectDB.close();
+            if (billDAO.connectDB != null) billDAO.connectDB.close();
         }
     }
 
