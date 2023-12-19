4a2c6cc5552258c044875c1c31ba5d878089f297
Trần Minh Tuyên <114179423+TranMinhTuyen-20130459@users.noreply.github.com>
2023-12-19 15:09:53 +0700

    cập nhật dữ liệu trong bảng account_admins

--- web_hoa_chat_thi_nghiem.sql
+++ web_hoa_chat_thi_nghiem.sql
@@ -39,10 +39,10 @@ CREATE TABLE `account_admin`  (
 -- ----------------------------
 -- Records of account_admin
 -- ----------------------------
-INSERT INTO `account_admin` VALUES ('nguyenphutai', 2, 1, '123456', '2023-11-29 11:28:42', '2023-11-29 11:28:42', 'Nguyễn Phú Tài');
-INSERT INTO `account_admin` VALUES ('nguyentandat', 2, 1, '123456', '2023-11-29 11:28:42', '2023-11-29 11:28:42', 'Nguyễn Tấn Đạt');
-INSERT INTO `account_admin` VALUES ('tranminhtuyen', 3, 1, '123456', '2023-11-29 11:28:42', '2023-11-29 11:28:42', 'Trần Minh Tuyên');
-INSERT INTO `account_admin` VALUES ('tuyenkun', 1, 1, '123456', '2023-11-29 11:28:42', '2023-11-29 11:28:42', NULL);
+INSERT INTO `account_admin` VALUES ('nguyenphutai', 2, 1, '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '2023-11-29 11:28:42', '2023-11-29 11:28:42', 'Nguyễn Phú Tài');
+INSERT INTO `account_admin` VALUES ('nguyentandat', 2, 1, '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '2023-11-29 11:28:42', '2023-11-29 11:28:42', 'Nguyễn Tấn Đạt');
+INSERT INTO `account_admin` VALUES ('tranminhtuyen', 3, 1, '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '2023-11-29 11:28:42', '2023-11-29 11:28:42', 'Trần Minh Tuyên');
+INSERT INTO `account_admin` VALUES ('tuyenkun', 1, 1, '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '2023-11-29 11:28:42', '2023-11-29 11:28:42', NULL);
 
 -- ----------------------------
 -- Table structure for account_customer
